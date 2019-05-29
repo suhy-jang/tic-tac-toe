fde52861d02014a5ee32c7c99b846fb26a74dd68
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-05-29 20:58:51 +0100

    add class documentation; make minor style changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,16 @@
+# frozen_string_literal: true
+
+# Game class handles all the game logic
+#   It accepts board_state, player & position as inputs via its move interface.
+#   Then validates the move & returns the resulting board state to the caller.
+#   It checks game status after every valid move, when game over;
+#   it updates @winner unless draw.
 class Game
   attr_reader :winner
 
   WINNING_PERMUTATIONS =
     [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].frezze
+     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
 
   def initialize
     @winner = nil
@@ -36,10 +43,25 @@ class Game
 
   def game_over?(board_state, player)
     #Check if game_over (winning or draw)
-    #Return { status: , winner: }
+    #Return { status: true/false, winner: player object / DRAW }
   end
 
   def update_winner(player)
     @winner = player
   end
+
+  def self.info
+    puts "-----------------------------------------------------------------".center(80)
+    puts ""
+    puts "Tic Tac Toe is a paper-and-pencil game for two players, X and O, ".center(80)
+    puts ""
+    puts "who take turns marking the spaces in a 3×3 grid.".center(80)
+    puts ""
+    puts "The player who succeeds in placing three of their marks in a ".center(80)
+    puts ""
+    puts "horizontal, vertical, or diagonal row wins the game.".center(80)
+    puts ""
+    puts "-----------------------------------------------------------------".center(80)
+  end
 end
+
