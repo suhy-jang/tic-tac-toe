f37b8fa24d51401a50ad9f3e2d1017e1e240a40c
shjang7 <lucy.sh.jang@gmail.com>
2019-05-29 16:13:51 +0100

    Build Game class

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+class Game
+  @valid
+  @turn
+
+  def initialize
+    player1 = Player.new
+    player2 = Player.new
+  end
+
+  def move
+    #
+  end
+
+  def set_winner
+    #TicTacToe class is calling this to determine winner
+  end
+end
