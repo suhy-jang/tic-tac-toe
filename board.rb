fc1e8e4ffcac2e094c5020ad2007bd458404df03
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-05-29 13:15:59 +0100

    implement #init and #update_state

--- board.rb
+++ board.rb
@@ -1,16 +1,17 @@
 class Board
   attr_reader :state
   def initialize()
-    
+    @state = (1..9).to_a
   end
 
   def display
-
+  
   end
   
   private
 
-  def update_state
-
+  def update_state(game_piece, position)
+    @state[position - 1] = game_piece
+    @state
   end
 end
