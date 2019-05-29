17107ae1fcaf2dd081863f20e9249ed4f98e09cd
shjang7 <lucy.sh.jang@gmail.com>
2019-05-29 14:57:45 +0100

    Make it bigger display

--- board.rb
+++ board.rb
@@ -7,11 +7,14 @@ class Board
 
   def display
     board=""
-    line="-------------\n".rjust(55)
-    row1="| #{state[0]} | #{state[1]} | #{state[2]} |\n".rjust(55)
-    row2="| #{state[3]} | #{state[4]} | #{state[5]} |\n".rjust(55)
-    row3="| #{state[6]} | #{state[7]} | #{state[8]} |\n".rjust(55)
-    board << line << row1 << line << row2 << line << row3 << line
+    line="+-------+-------+-------+\n".rjust(55)
+    wall="|       |       |       |\n".rjust(55)
+    row1="|   #{state[0]}   |   #{state[1]}   |   #{state[2]}   |\n".rjust(55)
+    row2="|   #{state[3]}   |   #{state[4]}   |   #{state[5]}   |\n".rjust(55)
+    row3="|   #{state[6]}   |   #{state[7]}   |   #{state[8]}   |\n".rjust(55)
+    board << line << wall << row1 << wall
+    board << line << wall << row2 << wall
+    board << line << wall << row3 << wall << line
     puts board
   end
 
@@ -22,6 +25,3 @@ class Board
     @state
   end
 end
-
-b=Board.new
-b.display
