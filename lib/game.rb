dc565dee68d08c3192e61cf9fb2fb9c1d1e869a5
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-05-29 18:26:05 +0100

    update Game class boilerplate

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,26 @@
 class Game
-  attr_reader :next_turn, :winner
+  attr_reader :winner
 
-  def initialize(user=0)
-    @next_turn=user
+  WINNING_PERMUTATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+		[1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ]
+  
+  def initialize
+    @winner = nil
   end
 
-  def move(player,grid_num)
+  def move(board:, player:, position:)
     #valid? or invalid? -> doing something
   end
 
   private
-  def game_over?
+
+  def game_over?(board:)
     #set_winner
   end
 
   def set_winner
+    #Use WINNING_PERMUTATIONS to determine winner
   end
 end
