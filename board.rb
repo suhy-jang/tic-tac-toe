912a6e1d71f712df2f68d992523e30d7f50348d9
shjang7 <lucy.sh.jang@gmail.com>
2019-05-29 14:46:28 +0100

    Add display

--- board.rb
+++ board.rb
@@ -1,13 +1,20 @@
 class Board
   attr_reader :state
+
   def initialize()
     @state = (1..9).to_a
   end
 
   def display
-  
+    board=""
+    line="-------------\n".rjust(55)
+    row1="| #{state[0]} | #{state[1]} | #{state[2]} |\n".rjust(55)
+    row2="| #{state[3]} | #{state[4]} | #{state[5]} |\n".rjust(55)
+    row3="| #{state[6]} | #{state[7]} | #{state[8]} |\n".rjust(55)
+    board << line << row1 << line << row2 << line << row3 << line
+    puts board
   end
-  
+
   private
 
   def update_state(game_piece, position)
@@ -15,3 +22,6 @@ class Board
     @state
   end
 end
+
+b=Board.new
+b.display
