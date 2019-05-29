61a286cfc358723a9a00ec948068769f358474dc
shjang7 <lucy.sh.jang@gmail.com>
2019-05-29 17:37:17 +0100

    Init for Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,19 @@
 class Game
-  @valid
-  @turn
+  attr_reader :next_turn, :winner
 
-  def initialize
-    player1 = Player.new
-    player2 = Player.new
+  def initialize(user=0)
+    @next_turn=user
   end
 
-  def move
-    #
+  def move(player,grid_num)
+    #valid? or invalid? -> doing something
+  end
+
+  private
+  def game_over?
+    #set_winner
   end
 
   def set_winner
-    #TicTacToe class is calling this to determine winner
   end
 end
