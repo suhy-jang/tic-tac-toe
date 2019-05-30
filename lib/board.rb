6a5f2d11e884ec51e65af1edd4913f04cd48b08c
shjang7 <lucy.sh.jang@gmail.com>
2019-05-30 10:23:18 +0100

    Add for checking validation

--- lib/board.rb
+++ lib/board.rb
@@ -4,14 +4,36 @@
 #   It maintains a state; that is grid positions and pieces on them.
 #   A consumer can view its state, update its state or call its display method.
 class Board
-  attr_reader :state
+  attr_reader :state, :bFull
+
+  BOARD_NUMBERS = [1,2,3,4,5,6,7,8,9]
+  FULL_NUMBER = 9
+
+  @curr_number
+  @bFull
 
   def initialize
+    @curr_number = 0
+    @bFull = false
     @state = (1..9).to_a
   end
 
   def update_state(game_piece, position)
     @state[position - 1] = game_piece
+    increase_curr_number
+    @bFull = (@curr_number == FULL_NUMBER)
+  end
+
+  def increase_curr_number
+    @curr_number += 1
+  end
+
+  def get_board_numbers
+    BOARD_NUMBERS
+  end
+
+  def is_valid_position?(position)
+    @state[position-1].class == Integer
   end
 
   def display
