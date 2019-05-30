f5f0e40af1d7a4823c06cf61353fc0dac16e589d
shjang7 <lucy.sh.jang@gmail.com>
2019-05-30 17:34:24 +0100

    Move main play into game obj

--- lib/game.rb
+++ lib/game.rb
@@ -6,70 +6,97 @@
 #   It checks game status after every valid move, when game over;
 #   it updates @winner unless draw.
 class Game
-  WINNING_PERMUTATIONS =
-    [[1,2,3],[4,5,6],[7,8,9],
+  WINNING_PERMUTATIONS = [
+    [1,2,3],[4,5,6],[7,8,9],
     [1,4,7],[2,5,8],[3,6,9],
-    [1,5,9],[3,5,7]].freeze
+    [1,5,9],[3,5,7]
+  ].freeze
 
   attr_reader :winner
-  @winner
-  @player
-  @position
 
-  def initialize(player)
-    @player = player
+  def initialize (user1, user2, board)
+    @player1 = user1
+    @player2 = user2
+    @board = board
     @winner = nil
-    @b = Board.new
+    @turn = 0
   end
 
-  def move(user, position_num)
-    arr = @b.get_board_numbers
-    unless arr.include?(position_num)
-      return false
+  def game_start
+    name = UserInterface::ask_who_first(@player1.name, @player2.name)
+    @turn = (@player1.name == name)? 0: 1
+    until is_board_full? || is_winner?
+      (0 == @turn)? play(@player1) : play(@player2)
+      @turn = (@turn+1)%2
+      puts "log: this turn is #{@turn}"
     end
-    @player = user
-    @position = position_num
-    update_board?
   end
 
-  def game_over?
-    over = false
-    over = true if @b.bFull
-    if is_winner?(@player)
-      set_winner(@player)
-      over = true
+  def play(player)
+    loop do
+      @board.display
+      position = UserInterface::ask_position(player.name)
+      #puts "log: ask again"
+      next unless board_valid?(position)
+      puts "log: before move"
+      move(player, position)
+      puts "log: after move"
+      break
     end
-    return over
+    puts "log: player one end"
+  end
+
+  def game_end
+    if @winner
+      UserInterface::inform_result(@winner)
+    else
+      UserInterface::inform_result
+    end
   end
 
   private
-
-  def update_board?
-    if @b.is_valid_position?(@position)
-      @b.update_state(@player, @position)
-      @b.display
-      return true
-    end
-    return false
+  def board_valid?(position)
+    t = @board.is_valid_position?(position)
+    puts "log: valid? #{t}"
+    t
   end
 
-  def is_winner?(player)
+  def is_board_full?
+    @board.bFull
+  end
+
+  def move(player, position)
+    puts "log: symbol is #{player.symbol}"
+    @board.update_state(player.symbol, position)
+    @board.display
+    winner_update(player, position)
+  end
+
+  def winner_update(player, position)
     bWin = false
     for line in WINNING_PERMUTATIONS
-      if line.include?(@position) &&
-            is_fill_same_user?(line,player)
+      if line.include?(position) &&
+            is_fill_same_user?(line, player)
         bWin = true
+        set_winner(player)
         break
       end
     end
+    puts "log: winner #{bWin}"
     bWin
   end
 
-  def is_fill_same_user?(idx_arr, user)
-    idx_arr.all? { |i| @b.state[i-1] == user }
+  #it seems able to improve speed
+  def is_winner?
+    !@winner.nil?
+  end
+
+  def is_fill_same_user?(idx_arr, player)
+    idx_arr.all? { |i| @board.state[i-1] == player.symbol }
   end
 
   def set_winner(player)
-    @winner = player
+    @winner = player.name
+    puts "log: set user with #{player.name}"
   end
 end
