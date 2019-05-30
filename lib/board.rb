b7a35029af3e726c81eeda408d43d6f25f35c5cb
shjang7 <lucy.sh.jang@gmail.com>
2019-05-30 17:32:52 +0100

    Update to fit with single purpose principle

--- lib/board.rb
+++ lib/board.rb
@@ -3,14 +3,13 @@
 # Board class handles all the board behaviours.
 #   It maintains a state; that is grid positions and pieces on them.
 #   A consumer can view its state, update its state or call its display method.
+require "./../lib/ui.rb"
+
 class Board
   attr_reader :state, :bFull
 
-  BOARD_NUMBERS = [1,2,3,4,5,6,7,8,9]
+  BOARD_NUMBERS = (1..9).to_a
   FULL_NUMBER = 9
-
-  @curr_number
-  @bFull
 
   def initialize
     @curr_number = 0
@@ -19,21 +18,40 @@ class Board
   end
 
   def update_state(game_piece, position)
+    puts "log: game_piece is #{game_piece}"
+    puts "log: position is #{position}"
     @state[position - 1] = game_piece
     increase_curr_number
-    @bFull = (@curr_number == FULL_NUMBER)
+    @bFull = curr_num_full?
   end
 
   def increase_curr_number
     @curr_number += 1
   end
 
-  def get_board_numbers
-    BOARD_NUMBERS
+  def curr_num_full?
+    @curr_number == FULL_NUMBER
   end
 
   def is_valid_position?(position)
-    @state[position-1].class == Integer
+    return false if out_of_board?(position)
+    return false unless empty_place?(position)
+    true
+  end
+
+  def out_of_board?(num)
+    bValid = !(BOARD_NUMBERS.include?(num))
+    puts "log: in board? #{BOARD_NUMBERS}"
+    UserInterface::throw_wrong_place_error unless bValid
+    bValid
+  end
+
+  def empty_place?(position)
+    bValid = false
+    bValid = true if @state[position-1].class == Integer
+    puts "log: empty? #{bValid}"
+    UserInterface::throw_occupied_error unless bValid
+    bValid
   end
 
   def display
