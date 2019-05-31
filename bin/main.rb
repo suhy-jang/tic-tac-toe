1e975363f3abc3d6e588cacf4e03895957c2703c
shjang7 <lucy.sh.jang@gmail.com>
2019-05-31 22:43:03 +0600

    Add log message for valid place

--- bin/main.rb
+++ bin/main.rb
@@ -10,7 +10,6 @@ def main
   UserInterface::inform_begin_message
   player1 = Player.new(UserInterface::ask_name(1), STONE_1)
   player2 = Player.new(UserInterface::ask_name(2), STONE_2)
-  puts "log: #{player1.name}, #{player2.name} is playing"
   board = Board.new
   game = Game.new(player1,player2,board)
   game.game_start
