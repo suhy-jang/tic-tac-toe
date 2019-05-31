69496abc1aae15d9891ad42942998291771af86f
shjang7 <lucy.sh.jang@gmail.com>
2019-05-31 17:22:22 +0600

    Add input again message

--- lib/ui.rb
+++ lib/ui.rb
@@ -9,20 +9,35 @@ module UserInterface
     puts border, line1, line2, line3, line4, border
   end
 
-  def self.ask_name
-    print "Type user's name: ".rjust(50)
-    gets.chomp
+  def self.ask_name(num)
+    user_input = ""
+    loop do
+      print "player #{num} name: ".rjust(50)
+      user_input = gets.chomp
+      unless "" == user_input
+        break
+      else
+        type_again
+      end
+    end
+    user_input
   end
 
-  def self.ask_first_player_name(p1_name, p2_name)
+  def self.type_again
+    puts "Please type again right value !!".rjust(50)
+  end
+
+  def self.ask_first_player_name(p1_name, p2_name, symbol)
     status = 0
+    puts "Who will do first?(#{symbol} stone)".rjust(50)
     until status > 0
-      print "Who will do first?(name): ".rjust(50)
+      print "Type the name: ".rjust(50)
       name = gets.chomp
       status = 1 if p1_name == name
       status = 2 if p2_name == name
+      type_again unless status > 0
     end
-    (1==status)? p1_name : p2_name
+    (1 == status)? p1_name : p2_name
   end
 
   def self.ask_position(name, stone)
@@ -31,25 +46,21 @@ module UserInterface
     gets.chomp.to_i
   end
 
-  def self.inform_occ_position(position)
-    puts "#{position} was already occupied".rjust(50)
-  end
-
   def self.inform_result(winner_name = nil, winner_stone = nil)
     puts "Game end".rjust(50)
     if winner_name.nil?
       puts "Tie".rjust(50)
     else
-      puts "[ '#{winner_name} (#{winner_stone})' Won! ]".rjust(50)
+      puts "[ '#{winner_name}' (#{winner_stone}) Won! ]".rjust(50)
     end
   end
 
-  def self.throw_wrong_place_error
-    puts "Please input between 1~9".rjust(50)
+  def self.throw_wrong_place_error(position = nil)
+   puts "#{position} already occupied!".rjust(50) if position
+   puts "Please input a correct (not occupied) number 1 and 9".rjust(50)
   end
 
-  def self.throw_occupied_error
-    puts "It is already occupied position".rjust(50)
-    puts "Please select another place".rjust(50)
+  def self.inform_success
+    puts "success! ".rjust(50)
   end
 end
