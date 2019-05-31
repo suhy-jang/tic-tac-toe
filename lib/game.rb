9720f2a4be6fae994f422a67483cadcc7199252d
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-05-31 16:04:14 +0100

    refactor game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,3 @@
-# frozen_string_literal: false
-
-# Game class handles all the game logic
-#   It accepts board_state, player & position as inputs via its move interface.
-#   Then validates the move & returns the resulting board state to the caller.
-#   It checks game status after every valid move, when game over;
-#   it updates @winner unless draw.
 class Game
   WINNING_PERMUTATIONS = [
     [1,2,3],[4,5,6],[7,8,9],
@@ -12,7 +5,7 @@ class Game
     [1,5,9],[3,5,7]
   ].freeze
 
-  def initialize (player1, player2, board)
+  def initialize(player1, player2, board)
     @player1 = player1
     @player2 = player2
     @board = board
@@ -23,8 +16,8 @@ class Game
   def game_start
     decide_first_player
     show_board
-    until @board.full? || is_winner?
-      (0 == @turn)? play(@player1) : play(@player2)
+    until @board.full? || @winner
+      @turn.zero? ? play(@player1) : play(@player2)
       change_turn
     end
     show_board
@@ -33,19 +26,27 @@ class Game
 
   private
 
+  def process_valid_response(player, position)
+    move(player, position)
+    show_board
+    UserInterface::inform_success
+  end
+
+  def process_invalid_response(response)
+    show_board
+    UserInterface::throw_wrong_place_error if response == 1
+    UserInterface::throw_wrong_place_error(position) if response == 2
+  end
+
   def play(player)
-    puts "log: this turn is '#{player.name}' "
+    puts "[Info] #{player.name}'s turn"
     loop do
       position = UserInterface::ask_position(player.name, player.stone)
       response = board_valid(position)
-      if response > 0
-        show_board
-        UserInterface::throw_wrong_place_error if 1 == response
-        UserInterface::throw_wrong_place_error (position) if 2 == response
-      else
-        move(player, position)
-        show_board
-        UserInterface::inform_success
+      show_board
+      process_invalid_response(response) unless response.zero?
+      if response.zero?
+        process_valid_response(player, position)
         break
       end
     end
@@ -54,6 +55,7 @@ class Game
 
   def decide_first_player
     name = UserInterface::ask_first_player_name(@player1.name, @player2.name, @player1.stone)
+
     unless compare(name, @player1.name)
       change_turn
       swap_stone(@player1, @player2)
@@ -61,11 +63,11 @@ class Game
   end
 
   def change_turn
-    @turn = (@turn + 1) % 2
+    @turn = @turn == 1 ? 0 : 1
   end
 
-  def swap_stone(p1, p2)
-    p1.stone, p2.stone = p2.stone, p1.stone
+  def swap_stone(player1, player2)
+    player1.stone, player2.stone = player2.stone, player1.stone
   end
 
   def compare(first, second)
@@ -77,54 +79,32 @@ class Game
   end
 
   def show_winner
-    if @winner
-      UserInterface::inform_result(@winner.name, @winner.stone)
-    else
-      UserInterface::inform_result
-    end
+    return UserInterface::inform_result(@winner.name, @winner.stone) if @winner
+    UserInterface::inform_result
   end
 
   def board_valid(position)
-    valid_message = 0
-    valid_message += 1 unless @board.number?(position)
-    if valid_message > 0
-      if @board.occupied?(position, @player1.stone, @player2.stone)
-        valid_message += 1
-      end
-    end
-    valid_message
+    return 1 unless @board.in_range?(position)
+    return 2 if @board.occupied?(position)
+    0
   end
 
   def move(player, position)
     puts "log: stone is #{player.stone}"
     @board.update_state(player.stone, position)
-    winner_status_update(player, position)
+    winner_status_update(player)
   end
 
-  def winner_status_update(player, position)
-    bWin = false
-    for line in WINNING_PERMUTATIONS
-      if line.include?(position) &&
-            is_fill_same_user?(line, player)
-        bWin = true
-        set_winner(player)
+  def winner_status_update(player)
+    player_positions = []
+    @board.each_with_index { |x, idx| player_positions << idx + 1 if x == player.stone }
+    WINNING_PERMUTATIONS.each do |winning_case|
+      if (player_positions & winning_case).length == 3
+        @winner = player
+        puts "log: set winner with #{player.name} (#{player.stone})"
         break
       end
     end
-    puts "log: winner status #{bWin}"
-    bWin
   end
 
-  def is_fill_same_user?(arr, player)
-    arr.all? { |i| @board.state[i-1] == player.stone }
-  end
-
-  def is_winner?
-    !@winner.nil?
-  end
-
-  def set_winner(player)
-    @winner = player
-    puts "log: set winner with #{player.name} (#{player.stone})"
-  end
 end
